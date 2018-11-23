3c913813a97c2376e29b3612165aa8c933ee6ba4
Massimiliano Masserelli <massimiliano.masserelli@essepi.eu>
2018-11-23 09:49:05 +0100

    Ordine corretto per i partner

--- inc/partners.php
+++ inc/partners.php
@@ -63,60 +63,60 @@
     });
     </script>';
 
- 		if ( catch_fullscreen_check_section( $enable_content ) ) {
- 			$headline       = get_theme_mod( 'catch_fullscreen_service_headline', esc_html__( 'Services', 'catch-fullscreen' ) );
- 			$subheadline    = get_theme_mod( 'catch_fullscreen_service_subheadline' );
- 			$image = get_theme_mod( 'catch_fullscreen_service_main_image' );
- 			$classes[] = 'section';
- 			$classes[] = 'ect-service';
-
- 			if ( $image ) {
- 				$output = '
- 				<div id="service-content-section" class="' . esc_attr( implode( ' ', $classes ) ) . '" style="background-image: url( ' . esc_url( $image ) . ' )">
- 					<div class="wrapper">';
- 			} else {
- 				$output = '
- 				<div id="service-content-section" class="' . esc_attr( implode( ' ', $classes ) ) . '">
- 					<div class="wrapper">';
- 			}
-
- 			if ( ! empty( $headline ) || ! empty( $subheadline ) ) {
- 				$output .= '<div class="section-heading-wrapper service-section-headline">';
-
- 				if ( ! empty( $headline ) ) {
- 					$output .= '<div class="section-title-wrapper"><h2 class="section-title">' . wp_kses_post( $headline ) . '</h2></div>';
- 				}
-
- 				if ( ! empty( $subheadline ) ) {
- 					$output .= '<div class="taxonomy-description-wrapper"><p class="section-subtitle">' . wp_kses_post( $subheadline ) . '</p></div>';
- 				}
-
- 				$output .= '
- 				</div><!-- .section-heading-wrapper -->';
- 			}
- 			$output .= '
- 				<div class="section-content-wrapper service-content-wrapper layout-four">';
-
- 			$output .= catch_fullscreen_post_page_category_service();
-
- 			$output .= '
- 						</div><!-- .partner-content-wrapper -->
- 				</div><!-- .wrapper -->
- 			</div><!-- #partners-content-section -->
-      <script type="text/javascript">
-      jQuery(document).ready(function($){
-      $(\'.partners-wrapper\').slick({
-        lazyLoad: \'ondemand\',
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        infinite: true,
-        });
-      });
-      </script>';
-
- 		}
+ 		// if ( catch_fullscreen_check_section( $enable_content ) ) {
+ 		// 	$headline       = get_theme_mod( 'catch_fullscreen_service_headline', esc_html__( 'Services', 'catch-fullscreen' ) );
+ 		// 	$subheadline    = get_theme_mod( 'catch_fullscreen_service_subheadline' );
+ 		// 	$image = get_theme_mod( 'catch_fullscreen_service_main_image' );
+ 		// 	$classes[] = 'section';
+ 		// 	$classes[] = 'ect-service';
+    //
+ 		// 	if ( $image ) {
+ 		// 		$output = '
+ 		// 		<div id="service-content-section" class="' . esc_attr( implode( ' ', $classes ) ) . '" style="background-image: url( ' . esc_url( $image ) . ' )">
+ 		// 			<div class="wrapper">';
+ 		// 	} else {
+ 		// 		$output = '
+ 		// 		<div id="service-content-section" class="' . esc_attr( implode( ' ', $classes ) ) . '">
+ 		// 			<div class="wrapper">';
+ 		// 	}
+    //
+ 		// 	if ( ! empty( $headline ) || ! empty( $subheadline ) ) {
+ 		// 		$output .= '<div class="section-heading-wrapper service-section-headline">';
+    //
+ 		// 		if ( ! empty( $headline ) ) {
+ 		// 			$output .= '<div class="section-title-wrapper"><h2 class="section-title">' . wp_kses_post( $headline ) . '</h2></div>';
+ 		// 		}
+    //
+ 		// 		if ( ! empty( $subheadline ) ) {
+ 		// 			$output .= '<div class="taxonomy-description-wrapper"><p class="section-subtitle">' . wp_kses_post( $subheadline ) . '</p></div>';
+ 		// 		}
+    //
+ 		// 		$output .= '
+ 		// 		</div><!-- .section-heading-wrapper -->';
+ 		// 	}
+ 		// 	$output .= '
+ 		// 		<div class="section-content-wrapper service-content-wrapper layout-four">';
+    //
+ 		// 	$output .= catch_fullscreen_post_page_category_service();
+    //
+ 		// 	$output .= '
+ 		// 				</div><!-- .partner-content-wrapper -->
+ 		// 		</div><!-- .wrapper -->
+ 		// 	</div><!-- #partners-content-section -->
+    //   <script type="text/javascript">
+    //   jQuery(document).ready(function($){
+    //   $(\'.partners-wrapper\').slick({
+    //     lazyLoad: \'ondemand\',
+    //     slidesToShow: 4,
+    //     slidesToScroll: 1,
+    //     autoplay: true,
+    //     autoplaySpeed: 2000,
+    //     infinite: true,
+    //     });
+    //   });
+    //   </script>';
+    //
+ 		// }
 
  		echo $output;
  	}
@@ -140,15 +140,16 @@
  		$output     = '';
 
  		$args = array(
- 			'orderby'             => 'post__in',
- 			'ignore_sticky_posts' => 1 // ignore sticky posts
+ 			'orderby'             => 'menu_order',
+      'order'               => 'ASC',
+ 			// 'ignore_sticky_posts' => 1 // ignore sticky posts
  		);
 
  		//Get valid number of posts
  		$args['post_type'] = 'cr3ativsponsor';
 
  		for ( $i = 1; $i <= $quantity; $i++ ) {
- 			$post_id = get_theme_mod( 'catch_fullscreen_service_cpt_' . $i );
+ 			$post_id = get_theme_mod( 'catch_fullscreen_partners_cpt_' . $i );
 
  			if ( $post_id && '' !== $post_id ) {
  				// Polylang Support.
@@ -161,7 +162,7 @@
  			}
  		}
 
- 		$args['post__in'] = $post_list;
+ 		// $args['post__in'] = $post_list;
 
  		$args['posts_per_page'] = $quantity;
 
@@ -205,7 +206,7 @@
         }
 
  				$output .= '
- 					<a class="post-thumbnail" href="' . esc_url( $splink ) . '" title="' . $title_attribute . '">
+ 					<a class="post-thumbnail" href="' . esc_url( $splink ) . '" title="' . $title_attribute . '" rel="nofollow" target="_blank">
  						'. $image . '
  					</a>';
 
@@ -214,7 +215,7 @@
  						<div class="entry-summary" style="font-size: small;"><p style="text-align: center;">' . get_the_excerpt() . '</p></div><!-- .entry-summary -->';
 
 
- 				$output .= the_title( '<header class="entry-header"><h2 class="entry-title"><a href="' . esc_url( $splink ) . '" rel="bookmark">', '</a></h2></header><!-- .entry-header -->', false );
+ 				$output .= the_title( '<header class="entry-header"><h2 class="entry-title"><a href="' . esc_url( $splink ) . '" rel="nofollow" target="_blank">', '</a></h2></header><!-- .entry-header -->', false );
 
 
  				$output .= '
